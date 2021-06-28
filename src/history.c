9fcd74a55b421cf84648a9727f4e9a4f80f4d9c6
Cristobal Chinolla <c.chinolla@ttu.edu>
2021-06-28 08:41:57 -0600

    added get, print, and free history with comments

--- src/history.c
+++ src/history.c
@@ -31,4 +31,41 @@ void add_history(List *list, char *str){
   }
 }
 
+
+// return pointer to specific token(word) if found, if not, returns NUll
+char *get_history(List *list, int id){
+  Item *temp = list->root;
+  while(temp != NULL){
+    if(temp->id == id){
+      return temp->str;
+    }
+    temp = temp->next;
+  }
+  return NULL; // returns null if no id is found
+}
+
+
+// traverses thru list printing out token id and string
+void print_history(List *list){
+  Item *temp = list->root;
+  printf("\n");
+  while(temp != NULL){
+    printf("ID: %d String: %s \n", temp->id, temp->str);
+    temp = temp->next;
+  }
+  printf("\n");
+}
+
+// frees history
+void free_history(List *list){
+  Item *temp;
+  while(list->root != NULL){
+    temp = list->root; //starts off at first node
+    list->root = list->root->next; // moves root to the next node
+    free(temp); // frees old root
+  }
+  free(list); // after all nodes are free, free the list
+}
+
+
   
